49cedd133766a1e708c1474d189443dc2665b217
canpool <canpool@163.com>
2026-01-22 14:13:27 +0800

    platform: sifli: hello lvgl v9
    Signed-off-by: canpool <canpool@163.com>

---
+++ src/platform/sifli/src/app_watch.c
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C) 2026 maminjie <canpool@163.com>
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+/* includes (local) ----------------------------------------------------------*/
+/* includes (standard library, system) ---------------------------------------*/
+#include <rtdevice.h>
+/* includes (other library) --------------------------------------------------*/
+#include "littlevgl2rtt.h"
+/* includes (project) --------------------------------------------------------*/
+
+/* defines -------------------------------------------------------------------*/
+#define APP_WATCH_GUI_TASK_STACK_SIZE (16 << 10)
+
+#define LCD_DEVICE_NAME "lcd"
+/* typedefs ------------------------------------------------------------------*/
+/* macro ---------------------------------------------------------------------*/
+/* functions (prototype/declaration) -----------------------------------------*/
+/* variables (extern) --------------------------------------------------------*/
+/* variables (local) ---------------------------------------------------------*/
+static struct rt_thread watch_thread;
+
+ALIGN(RT_ALIGN_SIZE)
+static uint8_t watch_thread_stack[APP_WATCH_GUI_TASK_STACK_SIZE];
+
+static rt_device_t lcd_device;
+
+/* variables (global) --------------------------------------------------------*/
+/* functions (inline) --------------------------------------------------------*/
+/* functions (implementation) ------------------------------------------------*/
+
+void app_watch_entry(void *parameter)
+{
+    rt_err_t ret = RT_EOK;
+    rt_uint32_t ms;
+
+    lcd_device = rt_device_find(LCD_DEVICE_NAME);
+    if (lcd_device == NULL) {
+        return;
+    }
+
+    ret = littlevgl2rtt_init(LCD_DEVICE_NAME);
+    if (ret != RT_EOK) {
+        return;
+    }
+
+    lv_obj_t *label = lv_label_create(lv_screen_active());
+    lv_label_set_text(label, "hello watchx on lvgl v9");
+    lv_obj_center(label);
+
+    while (1) {
+        ms = lv_task_handler();
+        rt_thread_mdelay(ms);
+    }
+}
+
+int app_watch_init(void)
+{
+    rt_err_t ret = RT_EOK;
+    rt_thread_t thread = RT_NULL;
+
+    ret = rt_thread_init(&watch_thread, "app_watch", app_watch_entry, RT_NULL, watch_thread_stack,
+                         APP_WATCH_GUI_TASK_STACK_SIZE, RT_THREAD_PRIORITY_MIDDLE,
+                         RT_THREAD_TICK_DEFAULT);
+
+    if (RT_EOK != ret) {
+        return RT_ERROR;
+    }
+    rt_thread_startup(&watch_thread);
+    return RT_EOK;
+}
+
+INIT_APP_EXPORT(app_watch_init);
