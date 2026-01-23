289a67e142233779404ebae8fbce07e75c105574
canpool <canpool@163.com>
2026-01-23 14:37:35 +0800

    page: home: hello adv_tileview
    Signed-off-by: canpool <canpool@163.com>

--- src/apps/launcher/home_page.c
+++ src/apps/launcher/home_page.c
@@ -33,7 +33,11 @@ static lv_obj_t *page_on_create(lv_adv_page_t *self)
     // page_ctx_t *ctx = (page_ctx_t *)self;
     lv_obj_t *scr = lv_adv_create_screen();
 
-    lv_obj_t *img = lv_image_create(scr);
+    lv_obj_t *tv = lv_adv_tileview_create(scr);
+
+    lv_obj_t *ca = lv_adv_tileview_area(tv, LV_ADV_TV_CENTER_AREA);
+
+    lv_obj_t *img = lv_image_create(ca);
     lv_image_set_src(img, &img_logo);
     // lv_obj_set_size(img, 100, 100);
     lv_adv_image_set_size(img, 200, 200);
@@ -41,10 +45,26 @@ static lv_obj_t *page_on_create(lv_adv_page_t *self)
 
     lv_font_t *font = watchx_font_create(48);
 
-    lv_obj_t *title = lv_label_create(scr);
+    lv_obj_t *title = lv_label_create(ca);
     lv_label_set_text(title, "Watchx");
     lv_obj_set_style_text_font(title, font, LV_PART_MAIN);
     lv_obj_align_to(title, img, LV_ALIGN_BOTTOM_MID, 0, 50);
+
+    lv_obj_t *ta = lv_adv_tileview_area(tv, LV_ADV_TV_TOP_AREA);
+    lv_obj_set_style_bg_opa(ta, LV_OPA_COVER, 0);
+    lv_obj_set_style_bg_color(ta, lv_color_hex(0xff0000), 0);
+
+    lv_obj_t *ba = lv_adv_tileview_area(tv, LV_ADV_TV_BOTTOM_AREA);
+    lv_obj_set_style_bg_opa(ba, LV_OPA_COVER, 0);
+    lv_obj_set_style_bg_color(ba, lv_color_hex(0x00ff00), 0);
+
+    lv_obj_t *la = lv_adv_tileview_area(tv, LV_ADV_TV_LEFT_AREA);
+    lv_obj_set_style_bg_opa(la, LV_OPA_COVER, 0);
+    lv_obj_set_style_bg_color(la, lv_color_hex(0xffff00), 0);
+
+    lv_obj_t *ra = lv_adv_tileview_area(tv, LV_ADV_TV_RIGHT_AREA);
+    lv_obj_set_style_bg_opa(ra, LV_OPA_COVER, 0);
+    lv_obj_set_style_bg_color(ra, lv_color_hex(0x00ffff), 0);
 
     return scr;
 }
