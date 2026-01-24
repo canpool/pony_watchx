879df3978a2c218ebf0f5d7e9f19dd715524a8e4
canpool <canpool@163.com>
2026-01-24 15:11:27 +0800

    platform: qt: support keypad
    Signed-off-by: canpool <canpool@163.com>

--- src/platform/simulator/qt/common/lv_main.c
+++ src/platform/simulator/qt/common/lv_main.c
@@ -9,6 +9,7 @@
 /* includes (other library) --------------------------------------------------*/
 /* includes (project) --------------------------------------------------------*/
 #include "watchx.h"
+#include "watchx_page.h"
 
 /* defines -------------------------------------------------------------------*/
 /* typedefs ------------------------------------------------------------------*/
@@ -20,11 +21,39 @@
 /* functions (inline) --------------------------------------------------------*/
 /* functions (implementation) ------------------------------------------------*/
 
+static void keypad_event_cb(lv_event_t *e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    uint32_t key = lv_indev_get_key(lv_indev_active());
+
+    switch (key) {
+    case LV_KEY_ENTER:
+        if (code == LV_EVENT_SHORT_CLICKED) {
+            if (lv_adv_page_is_current(LV_ADV_PAGE(home))) {
+                LV_ADV_PAGE_PUSH_ANIM(menu, LV_SCR_LOAD_ANIM_OUT_LEFT);
+            } else {
+                LV_ADV_PAGE_BACK_TO_ANIM(home, LV_SCR_LOAD_ANIM_OUT_RIGHT);
+            }
+        } else if (code == LV_EVENT_LONG_PRESSED_REPEAT) {
+            // TODO
+            LV_LOG_USER("ENTER long pressed repeat");
+        }
+        break;
+    }
+}
+
 static void lv_adv_ctx_init(void)
 {
     LV_LOG_USER("init");
 
     watchx_init();
+
+    lv_adv_page_t *home = lv_adv_page_get(LV_ADV_PAGE(home));
+    if (home) {
+        lv_group_add_obj(lv_adv_group_get_keypad(), home->scr);
+        lv_obj_add_event_cb(home->scr, keypad_event_cb, LV_EVENT_SHORT_CLICKED, NULL);
+        lv_obj_add_event_cb(home->scr, keypad_event_cb, LV_EVENT_LONG_PRESSED_REPEAT, NULL);
+    }
 }
 
 static void lv_adv_ctx_destroy(void)
