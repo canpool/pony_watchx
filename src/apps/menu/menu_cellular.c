ec6567d299685e367682b3b4a347a399364e7bf0
canpool <canpool@163.com>
2026-01-30 23:02:45 +0800

    optimize menu
    Signed-off-by: canpool <canpool@163.com>

--- src/apps/menu/menu_cellular.c
+++ src/apps/menu/menu_cellular.c
@@ -28,13 +28,10 @@ lv_obj_t *menu_cellular_create(lv_obj_t *parent, const menu_item_t *items, uint1
         const menu_item_t *item = items + i;
         lv_obj_t *btn = lv_adv_toolbutton_create(menu, item->icon, "");
         lv_adv_toolbutton_set_style(btn, LV_ADV_TOOLBUTTON_ICON_ONLY);
-        lv_obj_set_size(btn, 80, 80);
         lv_obj_set_style_radius(btn, LV_RADIUS_CIRCLE, 0);
         lv_obj_set_style_bg_color(btn, lv_color_hex(0x272727), 0);
         lv_obj_t *title = lv_adv_button_get_label(btn);
         lv_label_set_text(title, item->title);
-        lv_obj_t *icon = lv_adv_button_get_icon(btn);
-        lv_adv_image_set_size(icon, 72, 72);
         if (item->page != NULL) {
             lv_obj_add_event_cb(btn, menu_item_event_handler, LV_EVENT_SHORT_CLICKED, (void *)item->page);
         }
--- src/apps/menu/menu_list.c
+++ src/apps/menu/menu_list.c
@@ -24,7 +24,7 @@ lv_obj_t *menu_list_create(lv_obj_t *parent, const menu_item_t *items, uint16_t 
     lv_obj_t *lst = lv_adv_menu_list_create(parent);
     lv_adv_menu_list_set_type(lst, LV_ADV_MENU_LIST_NORMAL);
 
-    lv_obj_t *bar = lv_adv_menu_list_get_arc_bar(lst);
+    lv_obj_t *bar = lv_adv_menu_list_get_scrollbar(lst);
     lv_obj_set_style_arc_color(bar, lv_color_hex(0x505050), 0);
 
     for (size_t i = 0; i < item_cnt; i++) {
@@ -36,6 +36,7 @@ lv_obj_t *menu_list_create(lv_obj_t *parent, const menu_item_t *items, uint16_t 
         lv_obj_set_style_bg_color(btn, lv_color_hex(0x272727), 0);
         lv_obj_t *title = lv_adv_button_get_label(btn);
         lv_label_set_text(title, item->title);
+        lv_obj_set_style_text_font(title, WX_FONT(32), 0);
         lv_obj_t *icon = lv_adv_button_get_icon(btn);
         lv_adv_image_set_size(icon, 96, 96);
         if (item->page != NULL) {
