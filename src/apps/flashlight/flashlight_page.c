733f9b65bd19f0bb214fc3ebace97ca6bcbf0f34
canpool <canpool@163.com>
2026-02-06 19:30:15 +0800

    flashlight: add click-to-toggle on/off control
    Signed-off-by: canpool <canpool@163.com>

--- src/apps/flashlight/flashlight_page.c
+++ src/apps/flashlight/flashlight_page.c
@@ -14,7 +14,7 @@
 
 typedef struct {
     lv_adv_page_t base;
-    // TODO: add others
+    bool on;
 } page_ctx_t;
 
 /* macro ---------------------------------------------------------------------*/
@@ -25,11 +25,25 @@ typedef struct {
 /* functions (inline) --------------------------------------------------------*/
 /* functions (implementation) ------------------------------------------------*/
 
+static void event_handler(lv_event_t *e)
+{
+    lv_obj_t *obj = lv_event_get_current_target(e);
+    page_ctx_t *ctx = lv_event_get_user_data(e);
+
+    ctx->on = !ctx->on;
+
+    lv_color_t new_color = ctx->on ? lv_color_white() : lv_color_black();
+    lv_obj_set_style_bg_color(obj, new_color, LV_PART_MAIN);
+}
+
 static lv_obj_t *page_on_create(lv_adv_page_t *self)
 {
-    // page_ctx_t *ctx = (page_ctx_t *)self;
+    page_ctx_t *ctx = (page_ctx_t *)self;
+    ctx->on = true;
+
     lv_obj_t *scr = lv_adv_create_screen();
     lv_obj_set_style_bg_color(scr, lv_color_white(), LV_PART_MAIN);
+    lv_obj_add_event_cb(scr, event_handler, LV_EVENT_SHORT_CLICKED, ctx);
 
     return scr;
 }
